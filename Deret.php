b667135d464c77311fc3b01d0bc6b7741a817d17
Friana11 <127372377+Friana11@users.noreply.github.com>
2023-04-07 12:44:46 +0700

    Update Deret.php

--- Deret.php
+++ Deret.php
@@ -1,9 +1,9 @@
 <?php
 
-//Iqbal Heriyadi
-//D112111005
+//Friana Widya GUnawan
+//D112111022
 
-    for($i=1;$i<=30;$i++){
+    for($i=1;$i<=60;$i++){
         $a=0;
         for($j=1;$j<=$i;$j++){
             if($i % $j == 0){
